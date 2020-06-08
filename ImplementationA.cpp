c0779afd15466c32ffd892c6ad6afb85d7bbadad
Daniel Vu <danielvu2810@gmail.com>
2020-06-07 19:36:58 -0700

    run ring topology

--- ImplementationA.cpp
+++ ImplementationA.cpp
@@ -7,19 +7,49 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <string>
+#include <unistd.h>
 
-/*
-mpic++ -std=c++11 ImplementationA.cpp -o PartA
- 
- mpirun -np 4 ./PartA tc1.pbm DS_matrix.txt output.txt
- */
+using namespace std;
 
 /* Global variables, Look at their usage in main() */
 
-int matrix_length;
-int adj_matrix[1000][1000];
-int outputImage[1000][1000];
+int* calculateTotalHistogram(int* histogram,int* histogramInfo){
+    for(int i = 0; i < 257; i++){
+        histogram[i] += histogramInfo[i];
+    }
+    
+    return histogram;
+}
+
+int* countHistogram(int* histogramInfo,int* imageInfo,int chunkInfo){
+    int temp;
+    
+    for(int i = 0; i < 257; i++) histogramInfo[i] = 0;
+    
+    histogramInfo[0] += 1;
+        
+    for(int i = 0; i < chunkInfo; i++){
+        temp = histogramInfo[imageInfo[i] + 1];
+        temp += 1;
+        histogramInfo[imageInfo[i] + 1] = temp;
+    }
+
+    return histogramInfo;
+}
+
+int* updateVisited(int root, int* visited, int num_processes){
+    
+    MPI_Recv(visited,num_processes,MPI_INT,MPI_ANY_SOURCE,1,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    visited[root] = 1;
+    
+    for(int i = 0; i < num_processes;i ++){
+        MPI_Send(visited,num_processes,MPI_INT,i,1,MPI_COMM_WORLD);
+    }
+    
+    return visited;
+}
 
 /* **************** Change the function below if you need to ***************** */
 
@@ -28,6 +58,14 @@ int main(int argc, char* argv[])
     int processId, num_processes;
     int image_height, image_width, image_maxShades;
     int* inputImage;
+    int* adjMatrix;
+    int* chunkSizes;
+    int* visited;
+    
+    int* newhistogram = (int*) malloc(sizeof(int) * 257);
+    for(int i = 0; i < 257;i++){
+        newhistogram[i] = 0;
+    }
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &processId);
@@ -35,7 +73,7 @@ int main(int argc, char* argv[])
     
     if(argc != 4)
     {
-        if(processId == 2)
+        if(processId == 1)
         {
             std::cout << "ERROR: Incorrect number of arguments. Format is: <Input image filename> <Path To text file> <Output File Name>" << std::endl;
         }
@@ -43,7 +81,7 @@ int main(int argc, char* argv[])
         return 0;
     }
  
-    if (processId == 2){
+    if (processId == 1){
         std::ifstream file(argv[1]);
         if(!file.is_open())
         {
@@ -116,17 +154,139 @@ int main(int argc, char* argv[])
             }
         }
         
-        std::ifstream file(argv[2]);
-        if(!file.is_open())
+        chunkSizes = new int[num_processes];
+        
+        int total_pixels = image_height*image_width;
+        int size = floor(total_pixels*1.0/num_processes);
+        
+        for(int i = 0; i < num_processes; i++){
+            if(total_pixels > 0){
+                if(total_pixels >= size*2){
+                    chunkSizes[i] = size;
+                    total_pixels -= size;
+                }
+                else
+                {
+                    chunkSizes[i] = total_pixels;
+                    total_pixels = 0;
+                }
+            }
+        }
+        
+        adjMatrix = new int[num_processes*num_processes];
+
+        int temp;
+        std::string cell;
+        std::ifstream file2(argv[2]);
+        if(!file2.is_open())
         {
             std::cout << "ERROR: Could not open file " << argv[2] << std::endl;
             return 0;
         }
         else{
+            int t = 0;
+            while(std::getline(file2,workString))
+            {
+                std::stringstream stream(workString);
+                                    
+                while(std::getline(stream, cell, ' '))
+                {
+                    adjMatrix[t] = stoi(cell);
+                    t++;
+                }
+            }
+        }
+        
+    }
+    
+    int chunkInfo;
+    visited = (int*) malloc(sizeof(int) * num_processes);
+    
+    int* histogram = (int*) malloc(sizeof(int) * 257);
+    int* histogramInfo = (int*) malloc(sizeof(int) * 257);
+
+    for(int i = 0; i < num_processes; i++){
+        visited[i] = 0;
+    }
+    
+    int root = -1;
+    
+    MPI_Scatter(chunkSizes,1,MPI_INT,&chunkInfo,1,MPI_INT,1,MPI_COMM_WORLD);
+        
+    int* imageInfo = (int*) malloc(sizeof(int) * chunkInfo);
+    
+    MPI_Scatter(inputImage,chunkInfo,MPI_INT,imageInfo,chunkInfo,MPI_INT,1,MPI_COMM_WORLD);
+
+    int* matrixInfo = (int*) malloc(sizeof(int) * num_processes);
+
+    MPI_Scatter(adjMatrix,num_processes,MPI_INT,matrixInfo,num_processes,MPI_INT,1,MPI_COMM_WORLD);
+        
+    //if this is root node
+    if(processId == 1){
+        visited[processId] = 1;
+        
+        for(int i = 0; i < 257; i++){
+            histogram[i]=0;
+        }
+        
+        for(int i = 0; i < num_processes; i++){ //send to all neighbor
+            if(matrixInfo[i] == 1){
+                root = processId;
+                MPI_Send(&root,1,MPI_INT,i,0,MPI_COMM_WORLD);
+            }
+           
+            MPI_Send(visited,num_processes,MPI_INT,i,1,MPI_COMM_WORLD);
+        }
+        
+        // receive back from all neighbors and add its histogram
+        for(int i = 0; i < num_processes; i++){
+            if(i != 1){
+                MPI_Recv(histogramInfo,257,MPI_INT,MPI_ANY_SOURCE,3+i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            }
+            else{
+                histogramInfo = countHistogram(newhistogram,imageInfo,chunkInfo);
+            }
             
+            histogram = calculateTotalHistogram(histogram,histogramInfo);
+        }
+        
+        std::ofstream ofile(argv[3]);
+        
+        if (ofile.is_open()) {
+            for(int i = 1; i< 257; i++){
+                ofile <<histogram[i]<<endl;
+            }
+        }
+        else {
+            std::cout << "ERROR: Could not open output file " << argv[3] << std::endl;
+            return 0;
         }
     }
     
+    else {
+        int root;
+        
+        MPI_Recv(&root,1,MPI_INT,MPI_ANY_SOURCE,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        if(root != -1){
+            int has_neighbors = 0;
+            
+            visited = updateVisited(root,visited, num_processes);
+            
+            for(int i = 0; i < num_processes; i++){
+                
+                //traverse to next node if has neighbor and not visited
+                if(matrixInfo[i] == 1 && visited[i] != 1){
+                    has_neighbors = 1;
+                    MPI_Send(&processId,1,MPI_INT,i,0,MPI_COMM_WORLD);
+                }
+            }
+                            
+            // at the end, count the histogram and send back to the parent node
+            histogramInfo = countHistogram(histogramInfo,imageInfo,chunkInfo);
+            MPI_Send(histogramInfo,257,MPI_INT,1,3+processId,MPI_COMM_WORLD);
+        }
+    }
     
     MPI_Finalize();
     return 0;
--- ImplementationB.cpp
+++ ImplementationB.cpp
@@ -8,11 +8,13 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <cmath>
+using namespace std;
 const static int ARRAY_SIZE = 130000;
+using Words = char[16];
 using Lines = char[ARRAY_SIZE][16];
 
 /************* Feel free to change code below (even main() function), add functions, etc.. But do not change CL arguments *************/
-
 
 struct letter_only: std::ctype<char> 
 {
@@ -28,20 +30,20 @@ struct letter_only: std::ctype<char>
     }
 };
 
+int countFrequency(Words* words, int size, std::string target){
+    int freq = 0;
+    for (int i = 0; i < size; ++i){
+        if(words[i]){
+            if( !target.compare(words[i]) )
+                freq++;
+        }
+    }
+    return freq;
+}
+
 void DoOutput(std::string word, int result)
 {
     std::cout << "Word Frequency: " << word << " -> " << result << std::endl;
-}
-
-
-int countFrequency( std::vector<std::string> data, std::string word)
-{
-	int freq = 0;
-	for(auto workString : data) {
-		if( !workString.compare(word) )
-			freq++;
-	}
-	return freq;
 }
 
 int main(int argc, char* argv[])
@@ -50,6 +52,8 @@ int main(int argc, char* argv[])
     int numberOfProcesses;
     int *to_return = NULL;
     double start_time, end_time;
+    int numLines = 0;
+    int totalFreqs = 0;
  
     // Setup MPI
     MPI_Init( &argc, &argv );
@@ -66,8 +70,9 @@ int main(int argc, char* argv[])
         MPI_Finalize();
         return 0;
     }
+    
 	std::string word = argv[2];
- 
+    
     Lines lines;
     if (processId == 0) {
         std::ifstream file;
@@ -79,40 +84,71 @@ int main(int argc, char* argv[])
 			memset(lines[i], '\0', 16);
 			memcpy(lines[i++], workString.c_str(), workString.length());
 		}
+        
+        numLines = i;
     }
 	
     char buf[(ARRAY_SIZE / numberOfProcesses) * 16];
     
-    start_time=MPI_Wtime();
-	
-    std::vector<std::string> data;
-    for (int i = 0; i < (ARRAY_SIZE / numberOfProcesses) * 16; i += 16) {
-        char s[16];
-        memcpy(s, &buf[i], 16);
-        std::string s2(s);
-        data.push_back(s2);
-    }
-	
-	int temp_result = countFrequency(std::ref(data), word);
-	
-	std::string opt = argv[3];	
-	if( !opt.compare("b1") )
-	{
-		std::cout << "Using reduction" << std::endl;
-		to_return = new int;
 
-	} else {
-		std::cout << "Using ring" << std::endl;
-		
-		if (processId == 0){
-			to_return = new int;
-		}
-	}
-    // ... Eventually.. 
+    if(strcmp(argv[3], "b1") ==0 || strcmp(argv[3], "b2") == 0) {
+        int* chunkSizes = NULL;
+        
+        int chunkInfo;
+        
+        if(processId == 0){
+            chunkSizes = new int[numberOfProcesses];
+            
+            int words_per_chunk = ceil(numLines*1.0/numberOfProcesses);
+            
+            for(int i = 0; i < numberOfProcesses; i++){
+                chunkSizes[i] = words_per_chunk;
+            }
+        }
+        
+        MPI_Scatter(chunkSizes,1,MPI_INT,&chunkInfo,1,MPI_INT,0,MPI_COMM_WORLD);
+                            
+        Words* chunkWords = new Words[chunkInfo];
+        
+        MPI_Scatter(&lines[0][0], chunkInfo*16, MPI_CHAR, &chunkWords[0][0],chunkInfo*16,MPI_CHAR,0, MPI_COMM_WORLD);
+        
+        int chunkWordsFreq;
+        
+        if (processId == 0) {
+            start_time=MPI_Wtime();
+        }
+        
+        if (strcmp(argv[3],"b1") == 0){
+            chunkWordsFreq = countFrequency(chunkWords, chunkInfo, word);
+            
+            MPI_Reduce(&chunkWordsFreq,&totalFreqs,1,MPI_INT,MPI_SUM,0,MPI_COMM_WORLD);
+        }
+        else
+        {
+            chunkWordsFreq = countFrequency(chunkWords, chunkInfo, word);
+
+            if(numberOfProcesses == 1){
+                totalFreqs = chunkWordsFreq;
+            }
+            else
+            {
+                if (processId == 0){
+                    MPI_Send(&chunkWordsFreq, 1, MPI_INT, numberOfProcesses - 1, 0, MPI_COMM_WORLD);
+                    MPI_Recv(&totalFreqs, 1, MPI_INT, 1, MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+                }
+                else{
+                    MPI_Recv(&totalFreqs, 1, MPI_INT, (processId+1)%(numberOfProcesses),MPI_ANY_TAG, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+                    totalFreqs += chunkWordsFreq;
+                    MPI_Send(&totalFreqs,1,MPI_INT,processId-1,0,MPI_COMM_WORLD);
+                }
+            }
+        }
+    }
+
     if(processId == 0)
     {
-        DoOutput(word, *to_return);
-		end_time=MPI_Wtime();
+        DoOutput(word, totalFreqs);
+        end_time=MPI_Wtime();
         std::cout << "time: " << ((double)end_time-start_time) << std::endl;
     }
  
